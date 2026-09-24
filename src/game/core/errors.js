889fd7d08f3e88208b63a6f0eed5e39ab57e54b6
Rishaan Bhattacharya <rishaanb65@gmail.com>
2026-09-24 07:36:34 +0530

    Game errors done

---
+++ src/game/core/errors.js
@@ -0,0 +1,23 @@
+export class GameError extends Error {
+    constructor(message, code = 'GAME_ERROR', details = undefined) {
+        super(message);
+        this.name = this.constructor.name;
+        this.code = code;
+        this.details = details;
+
+    }
+}
+export class ValidationError extends Error {
+    constructor(message,details) {super(message, 'VALIDATION_ERROR', details);}
+}
+
+export class InventoryError extends GameError {
+    constructor(message,details) {super(message, 'INVENTORY_ERROR', details);}
+}
+export class QuestError extends GameError {
+    constructor(message,details)
+    {super(message, 'QUEST_ERROR', details);}
+}
+export class InteractionError extends GameError {
+    constructor(message,details) {super(message, 'INTERACTION_ERROR', details);}
+}
