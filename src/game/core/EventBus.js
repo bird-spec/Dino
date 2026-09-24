e2a24af95033b89b82af25dc02b67042166734e0
Rishaan Bhattacharya <rishaanb65@gmail.com>
2026-09-24 09:44:37 +0530

    EventBus developed

--- src/game/core/EventBus.js
+++ src/game/core/EventBus.js
@@ -1,44 +1,77 @@
 import { assertNonEmptyString} from "./utils.js";
 
-export class EventBus {Constructor(){
-    this.listener = new Map();
-}}
- on(eventName, listener)
-{
-    assertNonEmptyString(eventName, 'eventName');
-
-    if (typeof listener !== 'function') {
-        throw new TypeError('listener must be a function.');
+export class EventBus {
+    constructor() {
+        this.listeners = new Map();
     }
-    let listeners = this.listeners.get(eventName);
-    if (!listeners) {listeners = new Set();
-    this.listeners.set(eventName, listeners);}
 
-    listeners.add(listener);
-    return () => this.off(eventName, listener);
-}
-once(eventName, listener)
-{
-    const unsubscribe = this.on(eventName, (payload) => {
-        unsubscribe();
-        listener(payload);
-    });
+    on(eventName, listener) {
+        assertNonEmptyString(eventName, 'eventName');
 
-    return unsubscribe;
-}
-off (eventName, listener)
-{
-    const listeners = this.listeners.get(eventName)
-    if (!listeners) {
-        return false;
+        if (typeof listener !== 'function') {
+            throw new TypeError('listener must be a function.');
+        }
+        let listeners = this.listeners.get(eventName);
+        if (!listeners) {
+            listeners = new Set();
+            this.listeners.set(eventName, listeners);
+        }
+
+        listeners.add(listener);
+
+        return () => this.off(eventName, listener);
     }
-    const removed = listeners.delete(listener);
 
-    if (listeners.size === 0) {
-        this,listeners.delete(eventName)
+    once(eventName, listener) {
+        const unsubscribe = this.on(eventName, (payload) => {
+            unsubscribe();
+            listener(payload);
+        });
 
+        return unsubscribe;
     }
-    return removed;
+
+    off(eventName, listener)
+    {
+        const listeners = this.listeners.get(eventName)
+        if (!listeners) {
+            return false;
+        }
+        const removed = listeners.delete(listener);
+
+        if (listeners.size === 0) {
+            this.listeners.delete(eventName)
+
+        }
+        return removed;
+    }
+
+    emit(eventName, payload = {})
+    {
+        const listeners = this.listeners.get(eventName);
+        if (!listeners) {
+            return 0;
+        }
+        for (const listener of [...listeners]) {
+            try {
+                listener(payload);
+            } catch (error) {
+                queueMicrotask(() => {
+                    throw error;
+                });
+            }
+        }
+
+        return listeners.size;
+    }
+    clear(eventName) {
+        if(eventName === undefined) {
+            this.listeners.clear()
+            return;
+        }
+        this.listeners.delete(eventName);
+    }
 }
+
 
 
